5edc5d157378de317e5e75703520e55c78ed5885
Fresh <solovev-matveyka@inbox.ru>
2023-03-11 23:55:10 +0700

    Added music file and basic playing music

--- app/src/main/java/com/fresh/musicplayer/MainActivity.java
+++ app/src/main/java/com/fresh/musicplayer/MainActivity.java
@@ -2,13 +2,63 @@ package com.fresh.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView nowPlaying;
+
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nowPlaying = findViewById(R.id.nowPlaying);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMediaPlayer();
+    }
+
+    public void onControlPanelClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnStart:
+                runLocalMusic();
+                break;
+            case R.id.btnPause:
+                mediaPlayer.pause();
+                break;
+        }
+    }
+
+    public void runLocalMusic() {
+        Toast.makeText(this, "Проигрывается локальная музыка", Toast.LENGTH_SHORT).show();
+        mediaPlayer = MediaPlayer.create(this, R.raw.the_phoenix);
+        mediaPlayer.start();
+        nowPlaying.setText("Fall Out Boy - The Phoenix");
+    }
+
+    private void releaseMediaPlayer() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
