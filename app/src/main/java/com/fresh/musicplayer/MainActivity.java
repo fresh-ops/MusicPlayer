5fa2a56060e6d7ac4970c3ff2ff0649f0eab408f
Fresh <solovev-matveyka@inbox.ru>
2023-03-12 15:54:40 +0700

    Implemented radio listening Application can play one online radio station

--- app/src/main/java/com/fresh/musicplayer/MainActivity.java
+++ app/src/main/java/com/fresh/musicplayer/MainActivity.java
@@ -7,21 +7,18 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener {
     private TextView nowPlaying;
-    private Switch switchLoop;
 
     private MediaPlayer mediaPlayer;
-    private AudioManager audioManager;
 
     private final String musicLocation = "local/music";
+    private final String RADIO = "https://pub0301.101.ru:8443/stream/air/mp3/256/99";
     private PlaylistHolder playlistHolder;
 
     @Override
@@ -30,15 +27,11 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         setContentView(R.layout.activity_main);
 
         nowPlaying = findViewById(R.id.nowPlaying);
-        switchLoop = findViewById(R.id.switchLoop);
-        switchLoop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (mediaPlayer != null) mediaPlayer.setLooping(isChecked);
-            }
+        Switch switchLoop = findViewById(R.id.switchLoop);
+        switchLoop.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (mediaPlayer != null) mediaPlayer.setLooping(isChecked);
         });
 
-        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
         playlistHolder = new PlaylistHolder(musicLocation, getSongsList());
     }
 
@@ -51,7 +44,8 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
     public void onControlPanelClick(View view) {
         switch (view.getId()) {
             case R.id.btnStart:
-                runLocalMusic();
+                if (mediaPlayer != null) mediaPlayer.start();
+                else nowPlaying.setText("Выберите источник");
                 break;
             case R.id.btnPause:
                 mediaPlayer.pause();
@@ -92,25 +86,45 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
         }
     }
 
-    public void runLocalMusic() {
-        if (mediaPlayer == null) {
-            Toast.makeText(this, "Проигрывается локальная музыка", Toast.LENGTH_SHORT).show();
-            mediaPlayer = new MediaPlayer();
-            mediaPlayer.setOnPreparedListener(this);
-            mediaPlayer.setOnCompletionListener(this);
-            try {
-                AssetFileDescriptor afd = getAssets().openFd(playlistHolder.next());
-                mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-                afd.close();
-
-                mediaPlayer.prepare();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+    public void setContentSource(View view) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            mediaPlayer = null;
         }
-        else mediaPlayer.start();
-        mediaPlayer.setLooping(switchLoop.isChecked());
-        nowPlaying.setText("Локальная музыка");
+
+        switch (view.getId()) {
+            case R.id.btnRadio:
+                try {
+                    mediaPlayer = new MediaPlayer();
+
+                    mediaPlayer.setDataSource(RADIO);
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+
+                    nowPlaying.setText("Онлайн радио");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case R.id.btnLocal:
+                try {
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.setOnCompletionListener(this);
+
+                    AssetFileDescriptor afd = getAssets().openFd(playlistHolder.next());
+                    mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                    afd.close();
+
+                    mediaPlayer.prepare();
+
+                    nowPlaying.setText("Локальная музыка");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+        }
     }
 
     private String[]  getSongsList() {
@@ -154,4 +168,5 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnCom
     public void onPrepared(MediaPlayer mediaPlayer) {
         mediaPlayer.start();
     }
+
 }
